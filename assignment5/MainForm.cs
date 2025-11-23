740f76a9d822b20dc70fa202382153c1983605b4
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-22 19:51:01 -0500

    Add database

--- assignment5/MainForm.cs
+++ assignment5/MainForm.cs
@@ -19,6 +19,16 @@ namespace assignment5
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'personDBDataSet.Person' table. You can move, or remove it, as needed.
+            this.personTableAdapter.Fill(this.personDBDataSet.Person);
+
+        }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personDBDataSet);
 
         }
     }
